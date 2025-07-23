22fa692667757e0329a2019b14de2390ea298467
Luis Cruz <luissgczdev@gmail.com>
2025-07-23 00:06:28 -0300

    fix: corrigido erro de sintaxe no array de configuração de ambientes

--- database/phinx.php
+++ database/phinx.php
@@ -1,51 +1,47 @@
 <?php
 
-//Carregar o Composer
 require(__DIR__ . '/../vendor/autoload.php');
-
 
 $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/..');
 $dotenv->load();
 
-//Definir Fuso Horario do Aplicação
 date_default_timezone_set(getenv('APP_TIMEZONE'));
 
-return
-    [
-        'paths' => [
-            'migrations' => '%%PHINX_CONFIG_DIR%%/migrations',
-            'seeds' => '%%PHINX_CONFIG_DIR%%/seeds'
+return [
+    'paths' => [
+        'migrations' => '%%PHINX_CONFIG_DIR%%/migrations',
+        'seeds' => '%%PHINX_CONFIG_DIR%%/seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => getenv('APP_ENV'),
+        'production' => [
+            'adapter' => 'mysql',
+            'host' => getenv('DB_HOST_PHINX'),
+            'name' => getenv('DB_NAME_PHINX'),
+            'user' => getenv('DB_USER_PHINX'),
+            'pass' => getenv('DB_PASS_PHINX'),
+            'port' => getenv('DB_PORT_PHINX'),
+            'charset' => 'utf8',
         ],
-        'environments' => [
-            'default_migration_table' => 'phinxlog',
-            'default_environment' => getenv('APP_ENV'),
-            'production' => [
-                'adapter' => 'mysql',
-                'host' => getenv('DB_HOST_PHINX'),
-                'name' => getenv('DB_NAME_PHINX')],
-                'user' => getenv('DB_USER_PHINX'),
-                'pass' => getenv('DB_PASS_PHINX'),
-                'port' => getenv('DB_PORT_PHINX'),
-                'charset' => 'utf8',
-            ],
-            'development' => [
-                'adapter' => 'mysql',
-                'host' => getenv('DB_HOST_PHINX'),
-                'name' => getenv('DB_NAME_PHINX'),
-                'user' => getenv('DB_USER_PHINX'),
-                'pass' => getenv('DB_PASS_PHINX'),
-                'port' => getenv('DB_PORT_PHINX'),
-                'charset' => 'utf8',
-            ],
-            'testing' => [
-                'adapter' => 'mysql',
-                'host' => getenv('DB_HOST_PHINX'),
-                'name' => getenv('DB_NAME_PHINX'),
-                'user' => getenv('DB_USER_PHINX'),
-                'pass' => getenv('DB_PASS_PHINX'),
-                'port' => getenv('DB_PORT_PHINX'),
-                'charset' => 'utf8',
-            ]
+        'development' => [
+            'adapter' => 'mysql',
+            'host' => getenv('DB_HOST_PHINX'),
+            'name' => getenv('DB_NAME_PHINX'),
+            'user' => getenv('DB_USER_PHINX'),
+            'pass' => getenv('DB_PASS_PHINX'),
+            'port' => getenv('DB_PORT_PHINX'),
+            'charset' => 'utf8',
         ],
-        'version_order' => 'creation'
-    ];
+        'testing' => [
+            'adapter' => 'mysql',
+            'host' => getenv('DB_HOST_PHINX'),
+            'name' => getenv('DB_NAME_PHINX'),
+            'user' => getenv('DB_USER_PHINX'),
+            'pass' => getenv('DB_PASS_PHINX'),
+            'port' => getenv('DB_PORT_PHINX'),
+            'charset' => 'utf8',
+        ],
+    ],
+    'version_order' => 'creation',
+];
